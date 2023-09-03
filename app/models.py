20250dd15875f7038dc5d5cf7e92dbe135b632fd
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-09-03 23:33:58 +0300

    initial Restaurant and Customer models and create database

--- app/models.py
+++ app/models.py
@@ -3,4 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base=declarative_base()
 
-  
+class Restaurant:
+    __tablename__='restaurants'
+
+    id=Column(Integer(),primary_key=True)
+    name=Column(String())
+    price=Column(Integer())
+
+class Customer:
+    __tablename__='customers'
+
+    id=Column(Integer(),primary_key=True)
+    first_name=(String())
+    last_name=(String())    
